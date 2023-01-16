cab94c626e77f787381f6586cf68372d707cd966
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-16 23:28:41 +0700

    Add button and event listeners

--- javascript.js
+++ javascript.js
@@ -1,66 +1,87 @@
-//store rock, paper, and scissor in a variable
-const choice = ["rock", "paper", "scissor"];
+//store rock, paper, and scissors in a variable
+const choice = ["rock", "paper", "scissors"];
 let playerSelection;
 let computerSelection;
-let p1Score=0;
-let p2Score=0;
 
-//create function to play 5 rounds of rock, paper, and scissor game
-function game(){
-    for (let i=0;i<5;i++){
-        // prompt input message to choose rock, paper, or scissor and store the player's choice in a variable
-        playerSelection=prompt("Choose Rock, Paper, or Scissor?").toLowerCase();
+const buttons=document.querySelectorAll('button');
+let playerScore=document.querySelector(".playerScore");
+playerScore.textContent=0;
+let computerScore=document.querySelector(".computerScore");
+computerScore.textContent=0;
+let roundWinner=document.querySelector(".roundWinner");
+let gameWinner=document.querySelector(".gameWinner");
 
-        // Randomly return either rock, paper, or scissor for the computer's choice
-        computerSelection=choice[Math.floor(Math.random()*3)];
-        console.log(playRound(playerSelection,computerSelection));
-    }
-    
-   if(p1Score>p2Score){
-    console.log(`YOU WIN! CONGRATULATIONS!`)
-   } else if(p1Score<p2Score){
-    console.log(`YOU LOSE! TRY AGAIN!`)
-   } else {
-    console.log(`DRAW!`)
-   }
 
-   console.log(`Your Score: ${p1Score}`)
-   console.log(`YOur Opponent's Score: ${p2Score}`)
-}
+//create function to play 5 rounds of rock, paper, and scissors game
+// function game(){
+//    if(p1Score>p2Score){
+//     console.log(`YOU WIN! CONGRATULATIONS!`)
+//    } else if(p1Score<p2Score){
+//     console.log(`YOU LOSE! TRY AGAIN!`)
+//    } else {
+//     console.log(`DRAW!`)
+//    }
 
-function playRound(playerSelection, computerSelection){    
+//    console.log(`Your Score: ${p1Score}`)
+//    console.log(`YOur Opponent's Score: ${p2Score}`)
+// }
+
+function playRound(){  
+    playerSelection=this.classList.value;
+    computerSelection=choice[Math.floor(Math.random()*3)];
+
     //if player & computer choose the same choice, output DRAW
     if (playerSelection===computerSelection){
-        return `DRAW!`;
-
+        roundWinner.textContent="DRAW";
     //if player choose rock & computer choose paper, output LOSE
     } else if(playerSelection==="rock" && computerSelection==="paper"){
-        p2Score++;
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`;
+        computerScore.textContent++;
+        roundWinner.textContent= `You LOSE! ${computerSelection} beats ${playerSelection}`;
 
-    //if player choose rock & computer choose scissor, output WIN
-    } else if(playerSelection==="rock" && computerSelection==="scissor"){
-        p1Score++;
-        return `You WIN! ${playerSelection} beats ${computerSelection}`;
+    //if player choose rock & computer choose scissors, output WIN
+    } else if(playerSelection==="rock" && computerSelection==="scissors"){
+        playerScore.textContent++;
+        roundWinner.textContent=  `You WIN! ${playerSelection} beats ${computerSelection}`;
     
     //if player choose paper & computer choose rock, output WIN
     } else if(playerSelection==="paper" && computerSelection==="rock"){
-        p1Score++;
-        return `You WIN! ${playerSelection} beats ${computerSelection}`;
+        playerScore.textContent++;
+        roundWinner.textContent=  `You WIN! ${playerSelection} beats ${computerSelection}`;
 
-    //if player choose paper & computer choose scissor, output LOSE
-    } else if(playerSelection==="paper" && computerSelection==="scissor"){
-        p2Score++;
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`;
+    //if player choose paper & computer choose scissors, output LOSE
+    } else if(playerSelection==="paper" && computerSelection==="scissors"){
+        computerScore.textContent++;
+        roundWinner.textContent=  `You LOSE! ${computerSelection} beats ${playerSelection}`;
     
-    //if player choose scissor & computer choose rock, output LOSE
-    } else if(playerSelection==="scissor" && computerSelection==="rock"){
-        p2Score++;
-        return `You LOSE! ${computerSelection} beats ${playerSelection}`;
+    //if player choose scissors & computer choose rock, output LOSE
+    } else if(playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore.textContent++;
+        roundWinner.textContent=  `You LOSE! ${computerSelection} beats ${playerSelection}`;
     
-    //if player choose scissor & computer choose paper, output WIN
-    } else if(playerSelection==="scissor" && computerSelection==="paper"){
-        p1Score++;
-        return `You WIN! ${playerSelection} beats ${computerSelection}`;
-    }
+    //if player choose scissors & computer choose paper, output WIN
+    } else if(playerSelection==="scissors" && computerSelection==="paper"){
+        playerScore.textContent++;
+        roundWinner.textContent=  `You WIN! ${playerSelection} beats ${computerSelection}`;
+    };
+
+
+
+    if (playerScore.textContent===5){
+        gameWinner.textContent="YOU WIN THIS GAME!";
+        
+        playerScore.textContent=0;
+        computerScore.textContent=0;
+        roundWinner.textContent="";
+    } else if(computerScore.textContent===5){
+        gameWinner.textContent="YOU LOSE THIS GAME!";
+
+        playerScore.textContent=0;
+        computerScore.textContent=0;
+        roundWinner.textContent="";
+    };
+
 }
+
+
+for (const button of buttons) button.addEventListener('click', playRound);
+ 
